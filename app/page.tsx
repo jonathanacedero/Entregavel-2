366991f3c77c6cd258360f74350842b92846d699
Jonathan Acedero <jonathanacedero@ienh.com.br>
2026-09-15 13:12:56 +0000

    #

--- app/page.tsx
+++ app/page.tsx
@@ -22,32 +22,42 @@ export default function Home() {
       <h1>1- Botão com useState na Função</h1>
       <Botao />
       <hr />
+
       <h1>2- Botão sem useState na Função</h1>
       <BotaoSemUseState legado={botaoOn} onClick={() => setBotaoOn(!botaoOn)} />
       <hr />
+
       <h1>3- Campo nome</h1>
       <CampoNome />
       <hr />
+
       <h1>4- Cãompo de senha</h1>
       <CampoSenha />
       <hr />
+
       <h1>5- Cãompo de senha com useEffect</h1>
       <CampoSenhaUseEffect />
       <hr />
+
       <h1>6- Campo senha sem botãoo</h1>
       <CampoSenhaSemBotao />
       <hr />
+
       <h1>7- Inverter Texto</h1>
       <Inverter />
       <hr />
+
       <h1>8- Radio Button</h1>
       <RadioButton />
       <hr />
+
       <h1>9- Input select</h1>
       <InputSelect />
       <hr />
+
       <h1>10- Implementacao</h1>
       <Implementacao />
+      
     </main>
   );
 }
